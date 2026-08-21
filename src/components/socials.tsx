c639ecec65cf765d863635b0f0b1fe63789ee8e3
unknown <kyleninodescallar91@gmail.com>
2026-08-21 15:43:18 +0800

    updating the social links

--- src/components/socials.tsx
+++ src/components/socials.tsx
@@ -23,7 +23,8 @@ export default function Socials() {
       </a>
 
       <a
-        href="#"
+        href="https://github.com/kyledescallar"
+        target="_blank"
         title="GitHub"
         className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-200 border border-gray-300  transition hover:-translate-y-1"
       >
